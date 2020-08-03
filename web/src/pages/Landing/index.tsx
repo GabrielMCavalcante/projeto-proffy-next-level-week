8346f7f6bda9c4ad36f4d6d895a8154d459135bc
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-03 11:03:17 -0300

    Created Landing page component

---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+// Images
+import logo from 'assets/images/logo.svg'
+import landing from 'assets/images/landing.svg'
+import studyIcon from 'assets/images/icons/study.svg'
+import giveClassesIcon from 'assets/images/icons/give-classes.svg'
+import purpleHeartIcon from 'assets/images/icons/purple-heart.svg'
+
+// CSS styles
+import './styles.css'
+
+function Landing() {
+    return (
+        <div id="page-landing"> 
+            <div id="page-landing-content" className="container">
+                <div className="logo-container">
+                    <img src={logo} alt="Proffy"/>
+                    <h2>Sua plataforma de estudos online.</h2>
+                </div>
+                
+                <img src={landing} alt="Plataforma de estudos" className="hero-image"/>
+
+                <div className="buttons-container">
+                    <a href="self" className="study">
+                        <img src={studyIcon} alt="Estudar"/>
+                        Estudar
+                    </a>
+
+                    <a href="self" className="teach">
+                        <img src={giveClassesIcon} alt="Ensinar"/>
+                        Ensinar
+                    </a>
+                </div>
+
+                <span className="total-connections">
+                    Total de 200 conexões já realizadas 
+                    <img src={purpleHeartIcon} alt="Coração roxo"/>
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default Landing
