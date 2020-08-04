306ee13e99d707e58f449709e0aa4b1cf5504062
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-04 14:18:13 -0300

    Outsourced routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,9 @@
-console.log('Hello, world!')
+import express from 'express'
+import routes from './routes'
+
+const app = express()
+const PORT = 8080
+
+app.use(routes)
+
+app.listen(PORT, () => console.log('Server started @PORT=' + PORT))
