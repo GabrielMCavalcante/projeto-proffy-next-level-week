f9e4339d8bb63000c6260b2de08a203b9a03e6a4
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-05 08:24:15 -0300

    Added description prop

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -6,7 +6,10 @@ import PageHeader from 'components/PageHeader'
 function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container">
-            <PageHeader title="Que incrível que você quer ensinar." />
+            <PageHeader 
+                title="Que incrível que você quer ensinar." 
+                description="O primeiro passo é preencher esse formulário de inscrição."
+            />
         </div>
     )
 }
