df50c5d1a31f8731b0d2931b72068a1b2da3a089
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-05 08:27:16 -0300

    Added user data form

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -6,10 +6,32 @@ import PageHeader from 'components/PageHeader'
 function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container">
-            <PageHeader 
-                title="Que incrível que você quer ensinar." 
+            <PageHeader
+                title="Que incrível que você quer ensinar."
                 description="O primeiro passo é preencher esse formulário de inscrição."
             />
+
+            <main>
+                <form>
+                    <fieldset>
+                        <legend>Seus dados</legend>
+                        <div className="input-block">
+                            <label htmlFor="name">Nome completo</label>
+                            <input type="text" id="name" />
+                        </div>
+                        
+                        <div className="input-block">
+                            <label htmlFor="avatar">Avatar</label>
+                            <input type="text" id="avatar" />
+                        </div>
+
+                        <div className="input-block">
+                            <label htmlFor="whatsapp">WhatsApp</label>
+                            <input type="text" id="whatsapp" />
+                        </div>
+                    </fieldset>
+                </form>
+            </main>
         </div>
     )
 }
