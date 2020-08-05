4f9367c2e6f2c9cf97150a3dfeed2410ffc0f206
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-05 17:37:01 -0300

    Added custom feedback message

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -13,44 +13,51 @@ import purpleHeartIcon from 'assets/images/icons/purple-heart.svg'
 import './styles.css'
 
 function Landing() {
-
-    const [totalConnections, setTotalConnections] = useState(0)
+    const [feedback, setFeedback] = useState('Carregando conexões...')
+    const [error, setError] = useState(true)
 
     useEffect(() => {
-        (async function fetchConnections() {
-            const connections = (await axios.get('/connections')).data.total
-            setTotalConnections(connections)
+        (function fetchConnections() {
+            axios.get('/connections').then(response => {
+                const totalConnections = response.data.total
+                setError(false)
+                setFeedback(`
+                    Total de ${totalConnections}
+                    ${totalConnections === 1 ? "conexão" : "conexões"} já
+                    ${totalConnections === 1 ? "realizada" : "realizadas"}
+                `)
+            }).catch(() => {
+                setError(true)
+                setFeedback('Não foi possível recuperar o total de conexões :(')
+            })
         })()
     }, [])
 
     return (
-        <div id="page-landing"> 
+        <div id="page-landing">
             <div id="page-landing-content" className="container">
                 <div className="logo-container">
-                    <img src={logo} alt="Proffy"/>
+                    <img src={logo} alt="Proffy" />
                     <h2>Sua plataforma de estudos online.</h2>
                 </div>
-                
-                <img src={landing} alt="Plataforma de estudos" className="hero-image"/>
+
+                <img src={landing} alt="Plataforma de estudos" className="hero-image" />
 
                 <div className="buttons-container">
                     <Link to="/study" className="study">
-                        <img src={studyIcon} alt="Estudar"/>
+                        <img src={studyIcon} alt="Estudar" />
                         Estudar
                     </Link>
 
                     <Link to="/teach" className="teach">
-                        <img src={giveClassesIcon} alt="Ensinar"/>
+                        <img src={giveClassesIcon} alt="Ensinar" />
                         Ensinar
                     </Link>
                 </div>
 
                 <span className="total-connections">
-                    Total de&nbsp;
-                    {totalConnections}&nbsp; 
-                    {totalConnections === 1 ? "conexão" : "conexões"}&nbsp;já&nbsp; 
-                    {totalConnections === 1 ? "realizada" : "realizadas"} 
-                    <img src={purpleHeartIcon} alt="Coração roxo"/>
+                    {feedback}
+                    {!error && <img src={purpleHeartIcon} alt="Coração roxo" />}
                 </span>
             </div>
         </div>
