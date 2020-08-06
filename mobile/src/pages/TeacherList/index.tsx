cda72163fb8049644c7ca1856da5aba98337aa79
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-06 12:04:32 -0300

    Created initial TeacherList page component

---
+++ mobile/src/pages/TeacherList/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+// Styles
+import styles from './styles'
+
+function TeacherList() {
+    return (
+        <View style={styles.container}>
+            <Text>Teacher List</Text>
+        </View>
+    )
+}
+
+export default TeacherList
