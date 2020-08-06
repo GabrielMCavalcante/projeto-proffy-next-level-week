55106ad900c636f091466b159ce8de3ad1e1bd35
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-06 12:11:46 -0300

    Created initial GiveClasses page component

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function GiveClasses() {
+    return (
+        <View>
+            <Text>Give Classes</Text>
+        </View>
+    )
+}
+
+export default GiveClasses
