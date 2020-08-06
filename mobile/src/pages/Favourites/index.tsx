b22c263c8c870e1aa7d501bbd69860ba1fe7c4c8
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-06 15:07:12 -0300

    Added PageHeader component

--- mobile/src/pages/Favourites/index.tsx
+++ mobile/src/pages/Favourites/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+// Components
+import PageHeader from 'components/PageHeader'
+
 // Styles
 import styles from './styles'
 
 function Favourites() {
     return (
         <View style={styles.container}>
+            <PageHeader title="Meus Proffys Favoritos"/>
             <Text>Favourites</Text>
         </View>
     )
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+// Components
+import PageHeader from 'components/PageHeader'
+
 // Styles
 import styles from './styles'
 
 function TeacherList() {
     return (
         <View style={styles.container}>
+            <PageHeader title="Proffys Disponíveis"/>
             <Text>Teacher List</Text>
         </View>
     )
