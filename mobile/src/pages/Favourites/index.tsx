ea99ec55b453eac84c743fb12b6d6d50747948fd
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-06 14:14:25 -0300

    Created initial Favourites page component

---
+++ mobile/src/pages/Favourites/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+// Styles
+import styles from './styles'
+
+function Favourites() {
+    return (
+        <View style={styles.container}>
+            <Text>Favourites</Text>
+        </View>
+    )
+}
+
+export default Favourites
