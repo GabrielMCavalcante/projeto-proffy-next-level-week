b0b6b56c37cd8e70f5f88f40bb675a2bbf19cc1b
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-07 18:51:40 -0300

    Added styles for filter toggler button

--- mobile/src/components/PageHeader/styles.ts
+++ mobile/src/components/PageHeader/styles.ts
@@ -19,5 +19,39 @@ export default StyleSheet.create({
         lineHeight: 32,
         maxWidth: 160,
         marginVertical: 40
+    },
+
+    filtersContainer: {
+        alignItems: 'center',
+        marginBottom: 16
+    },
+
+    toggleFiltersButtonWrapper: {
+        width: '100%',
+        height: 50,
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        borderRadius: 8,
+        borderColor: 'black',
+        borderWidth: 2,
+        borderStyle: 'dashed'
+    },
+
+    toggleFiltersButton: {
+        height: '100%',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        flexDirection: 'row'
+    },
+
+    toggleFiltersButtonText: {
+        color: '#F0F0F4'
+    },
+
+    filtersInput: {
+        width: '100%',
+        flexDirection: 'row'
     }
 })
