c1517393a19f11e55912f1ea3f655700e306985f
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-07 18:53:08 -0300

    Created styles for filter form

--- mobile/src/pages/TeacherList/styles.ts
+++ mobile/src/pages/TeacherList/styles.ts
@@ -7,6 +7,74 @@ export default StyleSheet.create({
     },
 
     teacherList: {
-        marginTop: -60
+        marginTop: -40
+    },
+
+    searchForm: {
+        marginTop: 10,
+        marginBottom: 24,
+        width: '100%',
+        height: 250
+    },
+
+    label: {
+        marginTop: 12,
+        color: '#D4C2FF',
+        fontFamily: 'Poppins_400Regular'
+    },
+
+    input: {
+        height: 54,
+        backgroundColor: '#FFF',
+        borderRadius: 8,
+        justifyContent: 'center',
+        paddingHorizontal: 16,
+        marginTop: 4,
+        marginBottom: 16
+    },
+
+    inputGroup: {
+        // flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+
+    inputBlock: {
+        width: '100%',
+    },
+
+    timeContainer: {
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        flexDirection: 'row'
+    },
+
+    timeDisplay: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flex: 1,
+        height: 50
+    },
+
+    filterButton: {
+        // display: 'none',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 50,
+        backgroundColor: '#04D361',
+        borderRadius: 8,
+        marginTop: 15
+    },
+
+    filterButtonIcon: {
+        fontSize: 26,
+        color: '#F0F0F4',
+        marginRight: 16
+    },
+
+    filterButtonText: {
+        color: '#F0F0F4',
+        fontFamily: 'Archivo_700Bold'
     }
 })
