15f774e814c3a777f8417295ee776c6146f537eb
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-07 11:07:14 -0300

    Added ScrollView

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 
 // Components
 import PageHeader from 'components/PageHeader'
+import TeacherItem from 'components/TeacherItem'
 
 // Styles
 import styles from './styles'
@@ -10,8 +12,19 @@ import styles from './styles'
 function TeacherList() {
     return (
         <View style={styles.container}>
-            <PageHeader title="Proffys Disponíveis"/>
-            <Text>Teacher List</Text>
+            <PageHeader title="Proffys Disponíveis" />
+            <ScrollView 
+                style={styles.teacherList}
+                contentContainerStyle={{
+                    paddingHorizontal: 16,
+                    paddingBottom: 16
+                }}
+            >
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+            </ScrollView>
         </View>
     )
 }
