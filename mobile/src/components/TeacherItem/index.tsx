0cb4f026364e397aa423b2b95b5b2907a7ae4e24
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-07 21:09:37 -0300

    Added dynamic data props

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text, Image } from 'react-native'
 import { RectButton } from 'react-native-gesture-handler'
+import { SvgUri } from 'react-native-svg'
+import axios from '../../axios-config'
 
 // Images
 import favouriteHeartImg from 'assets/images/icons/heart-outline.png'
@@ -10,40 +12,69 @@ import whatsappImg from 'assets/images/icons/whatsapp.png'
 // Styles
 import styles from './styles'
 
-function TeacherItem() {
+interface TeacherItemProps {
+    teacherId: number,
+    teacherPhotoURL: string,
+    teacherName: string,
+    teacherSubject: string,
+    teacherDescriptionHeader: string,
+    teacherDescriptionContent: string,
+    teacherPrice: number,
+    teacherWhatsapp: number
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = props => {
+
+    function createConnection() {
+        axios.post('/connections', { user_id: props.teacherId })
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.profile}>
                 <View style={styles.profileHeader}>
-                    <Image
-                        style={styles.avatar}
-                        source={{ uri: `https://github.com/gabrielmcavalcante.png` }}
-                    />
+                    {
+                        props.teacherPhotoURL.endsWith('.png')
+                            ? (
+                                <Image
+                                    style={styles.avatar}
+                                    source={{ uri: props.teacherPhotoURL }}
+                                />
+                            ) 
+                            : (
+                                <SvgUri
+                                    style={styles.avatar}
+                                    uri={props.teacherPhotoURL}
+                                />
+                            )
+                    }
 
                     <View style={styles.profileInfo}>
-                        <Text style={styles.name}>Gabriel Cavalcante</Text>
-                        <Text style={styles.subject}>Programação</Text>
+                        <Text style={styles.name}>{props.teacherName}</Text>
+                        <Text style={styles.subject}>{props.teacherSubject}</Text>
                     </View>
                 </View>
 
-                <Text style={styles.bioHeader}>Entusiasta das melhores tecnologias de programação</Text>
-                <Text style={styles.bioContent}>Apaixonado por programar e resolver problemas do dia-a-dia com o poder da tecnologia</Text>
+                <Text style={styles.bioHeader}>{props.teacherDescriptionHeader}</Text>
+                <Text style={styles.bioContent}>{props.teacherDescriptionContent}</Text>
             </View>
 
             <View style={styles.footer}>
                 <Text style={styles.price}>
                     Preço/hora {'   '}
-                    <Text style={styles.priceValue}>R$ 20,00</Text>
+                    <Text style={styles.priceValue}>R$ {
+                        props.teacherPrice.toFixed(2).replace('.', ',')
+                    }</Text>
                 </Text>
 
                 <View style={styles.buttonsContainer}>
                     <RectButton style={[styles.favouriteButton, styles.favourited]}>
                         {/* <Image source={favouriteHeartImg}/> */}
-                        <Image source={unfavouriteHeartImg}/>
+                        <Image source={unfavouriteHeartImg} />
                     </RectButton>
 
                     <RectButton style={styles.contactButton}>
-                        <Image source={whatsappImg}/>
+                        <Image source={whatsappImg} />
                         <Text style={styles.contactButtonText}>
                             Entrar em contato
                         </Text>
