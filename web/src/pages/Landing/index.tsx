06969a92cab0b33bbe4f67a7c897ec8048d785ab
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-11 13:59:44 -0300

    Created Landing page for initial routing

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,67 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import axios from 'axios-config'
-
-// Images
-import logo from 'assets/images/logo.svg'
-import landing from 'assets/images/landing.svg'
-import studyIcon from 'assets/images/icons/study.svg'
-import giveClassesIcon from 'assets/images/icons/give-classes.svg'
-import purpleHeartIcon from 'assets/images/icons/purple-heart.svg'
-
-// CSS styles
-import './styles.css'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
 function Landing() {
-    const [feedback, setFeedback] = useState('Carregando conexões...')
-    const [error, setError] = useState(true)
+    
+    const [authenticated] = useState(false)
+    const history = useHistory()
+    debugger
+    if(authenticated) history.replace('/menu')
+    else history.replace('/auth')    
 
-    useEffect(() => {
-        (function fetchConnections() {
-            axios.get('/connections').then(response => {
-                const totalConnections = response.data.total
-                setError(false)
-                setFeedback(`
-                    Total de ${totalConnections}
-                    ${totalConnections === 1 ? "conexão" : "conexões"} já
-                    ${totalConnections === 1 ? "realizada" : "realizadas"}
-                `)
-            }).catch(() => {
-                setError(true)
-                setFeedback('Não foi possível recuperar o total de conexões :(')
-            })
-        })()
-    }, [])
-
-    return (
-        <div id="page-landing">
-            <div id="page-landing-content" className="container">
-                <div className="logo-container">
-                    <img src={logo} alt="Proffy" />
-                    <h2>Sua plataforma de estudos online.</h2>
-                </div>
-
-                <img src={landing} alt="Plataforma de estudos" className="hero-image" />
-
-                <div className="buttons-container">
-                    <Link to="/study" className="study">
-                        <img src={studyIcon} alt="Estudar" />
-                        Estudar
-                    </Link>
-
-                    <Link to="/teach" className="teach">
-                        <img src={giveClassesIcon} alt="Ensinar" />
-                        Ensinar
-                    </Link>
-                </div>
-
-                <span className="total-connections">
-                    {feedback}
-                    {!error && <img src={purpleHeartIcon} alt="Coração roxo" />}
-                </span>
-            </div>
-        </div>
-    )
+    return <></>
 }
 
 export default Landing
