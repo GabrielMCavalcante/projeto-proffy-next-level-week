88d65e7624211848a9a63d28c93209a303fe8f65
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-12 08:55:28 -0300

    Added SignupSuccess route

--- web/src/pages/Register/index.tsx
+++ web/src/pages/Register/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Route, Switch, Redirect } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Route, Switch, Redirect, useHistory } from 'react-router-dom'
 
 // Images
 import logo from 'assets/images/logo.svg'
@@ -7,21 +7,38 @@ import logo from 'assets/images/logo.svg'
 // Pages
 import Login from './Login'
 import Signup from './Signup'
+import SignupComplete from './Signup/SignupComplete'
 
 // CSS styles
 import './styles.css'
 
 function Register() {
+
+    const history = useHistory()
+    const [registerMethod, setRegisterMethod] = useState('login')
+
+    useEffect(() => {
+        if(history.location.pathname === '/auth/login')
+            setRegisterMethod('login')
+        else if(history.location.pathname === '/auth/cadastro') setRegisterMethod('signup')
+        else if(history.location.pathname === '/auth/cadastro/sucesso') setRegisterMethod('success')
+    }, [history.location.pathname])
+
     return (
-        <div id="page-register">
-            <div id="page-register-header">
-                <img src={logo} alt="Proffy" />
-                <h2>Sua plataforma de estudos online.</h2>
-            </div>
+        <div id="page-register" className={registerMethod === 'signup' ? 'invert' : ''}>
+            {
+                registerMethod !== 'success' && 
+                
+                <div id="page-register-header">
+                    <img src={logo} alt="Proffy" />
+                    <h2>Sua plataforma de estudos online.</h2>
+                </div>
+            }
             <div id="page-register-content">
                 <Switch>
                     <Route path="/auth/login" component={Login} />
-                    <Route path="/auth/cadastro" component={Signup} />
+                    <Route exact path="/auth/cadastro" component={Signup} />
+                    <Route path="/auth/cadastro/sucesso" component={SignupComplete} />
                     <Redirect to="/auth/login"/>
                 </Switch>
             </div>
