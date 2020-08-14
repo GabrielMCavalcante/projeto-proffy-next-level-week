4bbba768d2479a7e22f561fbb623283e299db1b1
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-13 20:26:42 -0400

    Created resetPassword function

--- server/src/controllers/AuthenticationController.ts
+++ server/src/controllers/AuthenticationController.ts
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import db from '../database/connection'
 import authConfig from '../config/auth'
+import sendMail from '../services/mail/sendmail'
 
 function generateToken(id: string, expiresIn = 86400) {
     return jwt.sign({ id }, authConfig.secret, { expiresIn })
@@ -13,81 +14,117 @@ export default class AuthenticationController {
 
         const parsedName = String(name).concat(' ', String(surname))
 
-        // Verifying if user email already exists in database
-        const response = await db('users').select('*').where('email', '=', email)
-        if (response.length > 0) {
-            // Email already exists
-            return res.status(400).json({ error: 'Este email já foi cadastrado.' })
-        } else {
-            // Email doesnt exists
-            bcrypt.genSalt(10, async (_, salt) => {
-                const hashPassword = await bcrypt.hash(password, salt)
+        try {
+            // Verifying if user email already exists in database
+            const response = await db('users').select('*').where('email', '=', email)
+            if (response.length > 0) {
+                // Email already exists
+                return res.status(400).json({ error: 'Este email já foi cadastrado.' })
+            } else {
+                // Email doesnt exists
+                bcrypt.genSalt(10, async (_, salt) => {
+                    const hashPassword = await bcrypt.hash(password, salt)
 
-                // Generate user id
-                const limit = String(email).length > String(password).length
-                    ? String(email).length
-                    : String(password).length
+                    // Generate user id
+                    const limit = String(email).length > String(password).length
+                        ? String(email).length
+                        : String(password).length
 
-                let userId = ''
+                    let userId = ''
 
-                for (let i = 0; i < limit; i++) {
-                    if (email.charAt(i)) {
-                        userId += email.charAt(i)
-                        if (password.charAt(i))
-                            userId += password.charAt(i)
-                    } else userId += password.charAt(i)
-                }
-
-                bcrypt.hash(userId, 5, async (err, hashId) => {
-                    if(err) return res.status(400).json({ error: 'Ocorreu um erro interno do servidor. Tente novamente mais tarde.' })
-                    
-                    const newUser = {
-                        __id: hashId,
-                        name: parsedName,
-                        email,
-                        password: hashPassword
+                    for (let i = 0; i < limit; i++) {
+                        if (email.charAt(i)) {
+                            userId += email.charAt(i)
+                            if (password.charAt(i))
+                                userId += password.charAt(i)
+                        } else userId += password.charAt(i)
                     }
 
-                    // Register user on database
-                    const registerUser = await db('users').insert(newUser)
+                    bcrypt.hash(userId, 5, async (err, hashId) => {
+                        if (err) return res.status(400).json({ error: 'Ocorreu um erro interno do servidor. Tente novamente mais tarde.' })
 
-                    if (registerUser) return res.status(200).send()
-                    else return res.status(400).json({ error: 'Um erro desconhecido ocorreu ao cadastrar o usuário. Tente novamente mais tarde.' })
+                        const newUser = {
+                            __id: hashId,
+                            name: parsedName,
+                            email,
+                            password: hashPassword
+                        }
+
+                        // Register user on database
+                        const registerUser = await db('users').insert(newUser)
+
+                        if (registerUser) return res.status(200).send()
+                        else return res.status(400).json({ error: 'Um erro desconhecido ocorreu ao cadastrar o usuário. Tente novamente mais tarde.' })
+                    })
                 })
-            })
+            }
+        } catch (err) {
+            return res.status(400).json({ error: 'Um erro interno ocorreu: ' + err })
         }
     }
 
     static async signin(req: Request, res: Response) {
         const { email, password } = req.body
 
-        // Finding user by email
-        const fetchUserPassword: { password: string }[] = await db('users').select('password').where('email', '=', email)
+        try {
+            // Finding user by email
+            const fetchUserPassword: { password: string }[] = await db('users')
+                .select('password').where('email', '=', email)
 
-        if (fetchUserPassword.length === 0) {
-            // User not found
-            res.status(400).json({ error: 'Não foi encontrado nenhum usuário com o email informado.' })
-        } else {
-            // User found
-            const fetchedPassword = fetchUserPassword[0].password
+            if (fetchUserPassword.length === 0) {
+                // User not found
+                res.status(400).json({
+                    error: 'Não foi encontrado nenhum usuário com o email informado.'
+                })
+            } else {
+                // User found
+                const fetchedPassword = fetchUserPassword[0].password
 
-            // Comparing recieved password with stored one
-            bcrypt.compare(password, fetchedPassword, async (err, same) => {
-                if (err) return res.status(400).json({ error: 'Ocorreu um erro interno no servidor. Tente novamente mais tarde.' })
-                else if (!same) return res.status(400).json({ error: 'Senha incorreta. Tente novamente' })
-                else {
-                    // Passwords are equal, proceeding to signin user
-                    const user = await 
-                        db('users')
-                        .select('__id', 'name', 'avatar', 'email', 'whatsapp', 'bio')
-                        .where('email', '=', email).first()
-                    
-                    const token = generateToken(user.__id)
+                // Comparing recieved password with stored one
+                bcrypt.compare(password, fetchedPassword, async (err, same) => {
+                    if (err) return res.status(400).json({ error: 'Ocorreu um erro interno no servidor. Tente novamente mais tarde.' })
+                    else if (!same) return res.status(400).json({ error: 'Senha incorreta. Tente novamente' })
+                    else {
+                        // Passwords are equal, proceeding to signin user
+                        const user = await
+                            db('users')
+                                .select('__id', 'name', 'avatar', 'email', 'whatsapp', 'bio')
+                                .where('email', '=', email).first()
 
-                    return res.status(200).json({ user, token })
-                }
-            })
+                        const token = generateToken(user.__id)
+
+                        return res.status(200).json({ user, token })
+                    }
+                })
+            }
+        } catch (err) {
+            return res.status(400).json({ error: 'Ocorreu um erro: ' + err })
         }
+    }
 
+    static async resetPassword(req: Request, res: Response) {
+        const { email } = req.body
+        
+        try {
+            // Verifying if user exists
+            const user = await db('users')
+                .select('__id').where('email', '=', email).first()
+
+            if (!user)
+                return res.status(400)
+                    .json({ error: 'Nenhum usuário com este email foi encontrado.' })
+            
+            const recoveryToken = generateToken(email, 90)
+
+            sendMail(email, recoveryToken)
+                .then(() => res.status(200).send())
+                .catch(err => res.status(400).json({ error: 'Um erro interno ocorreu: ' + err }))
+        } catch (err) {
+            return res.status(400).json({ error: 'Um erro interno ocorreu: ' + err })
+        }
+    }
+
+    static async updatePassword(req: Request, res: Response) {
+        
     }
 }
