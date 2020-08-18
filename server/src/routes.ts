2d64901b94f8d8fff14388780807e9f605658da1
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-18 12:30:10 -0400

    Added index route for ProfileController

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,6 +5,7 @@ import cors from "cors"
 import ClassesController from "./controllers/ClassesController"
 import ConnectionsController from "./controllers/ConnectionsController"
 import AuthenticationController from "./controllers/AuthenticationController"
+import ProfileController from "./controllers/ProfileController"
 
 // Middlewares
 import AuthMiddleware from "./middlewares/auth"
@@ -19,6 +20,9 @@ routes.post("/auth/signup", AuthenticationController.signup)
 routes.post("/auth/signin", AuthenticationController.signin)
 routes.post("/auth/password/reset", AuthenticationController.resetPassword)
 routes.put("/auth/password/reset/update", AuthenticationController.updatePassword)
+
+// Profile
+routes.get("/get-profile", AuthMiddleware, ProfileController.index)
 
 // Classes
 routes.get("/classes", AuthMiddleware, ClassesController.index)
