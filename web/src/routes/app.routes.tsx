2a913b4f6f4895b3903558edfdc60a149efd5596
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-26 07:16:27 -0400

    Added Profile route

--- web/src/routes/app.routes.tsx
+++ web/src/routes/app.routes.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import { Route, Redirect } from 'react-router-dom'
+import { Route, Redirect, Switch } from 'react-router-dom'
 
 // Pages
 import Menu from 'pages/Menu' 
+import Profile from 'pages/Profile'
 import TeacherList from 'pages/TeacherList'
 import TeacherForm from 'pages/TeacherForm'
 
 function AppRoutes() {
     return (
-        <>
+        <Switch>
             <Route path="/menu" component={Menu} />
+            <Route path="/profile" component={Profile} />
             <Route path="/study" component={TeacherList} />
             <Route path="/teach" component={TeacherForm} />
             <Redirect to="/menu"/>
-        </>
+        </Switch>
     )
 }
 
