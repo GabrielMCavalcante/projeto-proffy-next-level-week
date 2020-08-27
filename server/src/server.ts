28e67b5a86d4ed3e82a133e431cbc3f1f004bff5
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-27 14:55:40 -0400

    Increased request body size limit

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,13 @@ import path from "path"
 import dotenv from "dotenv"
 dotenv.config({path: path.resolve(__dirname, ".env")})
 import express from "express"
+import bodyParser from 'body-parser'
 import routes from "./routes"
 
 const app = express()
 const PORT = 3333
 
+app.use(bodyParser.json({limit: '300kb'}))
 app.use(routes)
 
 app.listen(PORT, () => console.log("Server started @PORT=" + PORT))
