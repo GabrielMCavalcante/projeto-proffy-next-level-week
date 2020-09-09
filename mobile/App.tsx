f1bb596f5bdbdb29c39bd7cabc1e249cff5a641b
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-09-09 14:22:10 -0400

    Added Onboarding route

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet } from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage'
 
 // Expo
 import { AppLoading } from 'expo'
 import { StatusBar } from 'expo-status-bar'
+
+// Pages
+import Onboarding from 'pages/Onboarding'
 
 // Routes
 import AppStack from 'routes/AppStack'
@@ -17,6 +21,9 @@ import {
 import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
 
 export default function App() {
+  const [component, setComponent] = useState<JSX.Element>(<AppStack />)
+  const [recheck, setRecheck] = useState(false)
+
   const [fontsLoaded] = useFonts({
     Archivo_400Regular,
     Archivo_700Bold,
@@ -24,11 +31,27 @@ export default function App() {
     Poppins_600SemiBold
   })
 
+  function onOnboardingDismiss() {
+    AsyncStorage.setItem('proffy:app:alreadyopened', 'true')
+      .then(() => setRecheck(true))
+  }
+
+  useEffect(() => {
+    (async function checkFirstAppOpen() {
+      if (fontsLoaded || recheck) {
+        const check = await AsyncStorage.getItem('proffy:app:alreadyopened')
+        if (!check) setComponent(<Onboarding onDismiss={onOnboardingDismiss}/>)
+        else setComponent(<AppStack />)
+      }
+    })()
+  }, [fontsLoaded, recheck])
+
   if (!fontsLoaded) return <AppLoading />
-  else return (
+
+  return (
     <>
       <StatusBar style="light" />
-      <AppStack />
+      {component}
     </>
   )
 }
