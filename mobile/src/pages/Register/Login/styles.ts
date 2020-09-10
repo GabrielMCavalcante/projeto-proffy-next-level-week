4671ba5aaddf103ef10bc9bcc05d0b2e01e27819
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-09-10 14:03:12 -0400

    Created Login page styles

---
+++ mobile/src/pages/Register/Login/styles.ts
@@ -0,0 +1,138 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    login: {
+        flex: 1
+    },
+
+    inner: {
+        flex: 1,
+        justifyContent: "flex-end"
+    },
+
+    loginImageWrapper: {
+        flex: 1,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#A380F6'
+    },
+
+    loginImage: {
+        resizeMode: 'contain',
+        height: '100%'
+    },
+
+    loginForm: {
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        width: '100%',
+    },
+
+    formHeader: {
+        flexDirection: 'row',
+        width: '80%',
+        marginTop: 20,
+        marginBottom: 35,
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+
+    formHeaderTitle: {
+        fontFamily: 'Poppins_600SemiBold',
+        fontSize: 23
+    },
+
+    createAccountBtnText: {
+        color: '#8257E5',
+        fontSize: 13
+    },
+
+    formFields: {
+        width: '80%'
+    },
+
+    formFieldGroup: {
+        width: '100%',
+        height: 60,
+        position: 'relative',
+        marginBottom: -1.5
+    },
+
+    formField: {
+        width: '100%',
+        height: '100%',
+        borderColor: '#E6E6F0',
+        borderWidth: 2,
+        paddingHorizontal: 16
+    },
+
+    firstField: {
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10
+    },
+
+    lastField: {
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10
+    },
+
+    inputIcon: {
+        position: 'absolute',
+        right: 10,
+        top: '30%'
+    },
+
+    loginActions: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '80%',
+        marginTop: 25
+    },
+
+    rememberUser: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+
+    rememberUserText: {
+        color: '#9C98A6',
+        marginLeft: 10
+    },
+
+    forgotPasswordBtnText: {
+        color: '#9C98A6'
+    },
+
+    submitLoginBtn: {
+        width: '80%',
+        height: 60,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 8,
+        marginTop: 26,
+        marginBottom: 26
+    },
+
+    submitLoginBtnUnactive: {
+        backgroundColor: '#DCDCE5'
+    },
+
+    submitLoginBtnActive: {
+        backgroundColor: '#04D361'
+    },
+
+    submitLoginBtnText: {
+        fontFamily: 'Archivo_700Bold'
+    },
+
+    submitLoginBtnTextUnactive: {
+        color: '#9C98A6'
+    },
+
+    submitLoginBtnTextActive: {
+        color: '#fff'
+    },
+})
