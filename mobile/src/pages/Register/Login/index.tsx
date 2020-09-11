9fbf16896f2140b4ed144ce99acd6b5803790513
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-09-11 18:09:45 -0400

    Added navigation and login logic

--- mobile/src/pages/Register/Login/index.tsx
+++ mobile/src/pages/Register/Login/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     KeyboardAvoidingView,
     View,
@@ -6,12 +6,19 @@ import {
     Image,
     TextInput,
     Platform,
-    SafeAreaView
+    SafeAreaView,
+    ActivityIndicator
 } from 'react-native'
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler'
 
+// Navigation
+import { useNavigation } from '@react-navigation/native'
+
 // Components
 import Checkbox from 'components/UI/Checkbox'
+
+// Contexts
+import { useAuth } from 'contexts/auth'
 
 // Images
 import backgroundImg from 'assets/images/login-background.png'
@@ -21,10 +28,95 @@ import hidePasswordImg from 'assets/images/icons/hide-password.png'
 // CSS styles
 import styles from './styles'
 
+// Interfaces
+import { Fields } from 'interfaces/index'
+
+const initialFields: Fields = {
+    email: {
+        value: '',
+        validation: /^[a-z-_\d.]{3,}@[a-z]{3,}(\.com|\.br|\.com\.br)$/i,
+        valid: false,
+        touched: false
+    },
+
+    password: {
+        value: '',
+        validation: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,30}$/,
+        valid: false,
+        touched: false
+    }
+}
+
 function Login() {
 
     const [showPassword, setShowPassword] = useState(false)
-    const [rememberMe, setRememberMe] = useState(false)
+    const [rememberUser, setRememberUser] = useState(false)
+    const [fields, setFields] = useState(initialFields)
+    const [formValid, setFormValid] = useState(false)
+    const [feedback, setFeedback] = useState('')
+
+    const authContext = useAuth()
+
+    const navigation = useNavigation()
+
+    useEffect(() => {
+        navigation.addListener('focus', () => {
+            setFields(initialFields)
+            setFormValid(false)
+        })
+    }, [navigation])
+
+    // useEffect(() => {
+    //     if(authContext.signedIn)
+    //         navigation.navigate("landing")
+    // }, [authContext.signedIn])
+
+    function onInputValueChange(newInputValue: string, inputIdentifier: string) {
+        const allFields = Object.keys(fields)
+
+        let isFormValid = true
+        const isInputValid = fields[inputIdentifier].validation.test(newInputValue)
+
+        if (isInputValid) {
+            allFields.forEach(field => {
+                if (isFormValid)
+                    if (field !== inputIdentifier)
+                        isFormValid = fields[field].validation.test(fields[field].value)
+            })
+        } else isFormValid = false
+
+        if (isFormValid !== formValid)
+            setFormValid(isFormValid)
+
+        if (feedback) setFeedback('')
+
+        setFields({
+            ...fields,
+            [inputIdentifier]: {
+                ...fields[inputIdentifier],
+                value: newInputValue,
+                touched: true,
+                valid: isInputValid
+            }
+        })
+    }
+
+    function setInputClasses(inputIdentifier: string) {
+        return [styles.formField, !fields[inputIdentifier].valid && fields[inputIdentifier].touched && styles.invalid]
+    }
+
+    async function login() {
+        if (feedback) setFeedback('')
+        const userAccount = {
+            email: fields.email.value,
+            password: fields.password.value,
+            rememberUser
+        }
+
+        const response = await authContext.signIn(userAccount)
+        if (typeof response === 'string') setFeedback(response)
+        console.log("[0]: Usuario logado?", authContext.signedIn)
+    }
 
     return (
         <KeyboardAvoidingView
@@ -40,7 +132,7 @@ function Login() {
                     <View style={styles.loginForm}>
                         <View style={styles.formHeader}>
                             <Text style={styles.formHeaderTitle}>Fazer login</Text>
-                            <BorderlessButton>
+                            <BorderlessButton onPress={() => navigation.navigate("signup")}>
                                 <Text style={styles.createAccountBtnText}>
                                     Criar uma conta
                                 </Text>
@@ -50,19 +142,23 @@ function Login() {
                         <View style={styles.formFields}>
                             <View style={styles.formFieldGroup}>
                                 <TextInput
-                                    style={[styles.formField, styles.firstField]}
+                                    style={[...setInputClasses("email"), styles.firstField]}
                                     placeholder="E-mail"
+                                    onChangeText={text => onInputValueChange(text, "email")}
+                                    value={fields.email.value}
                                 />
                             </View>
 
                             <View style={styles.formFieldGroup}>
                                 <TextInput
                                     secureTextEntry={!showPassword}
-                                    style={[styles.formField, styles.lastField]}
+                                    style={[...setInputClasses("password"), styles.lastField]}
                                     placeholder="Senha"
+                                    onChangeText={text => onInputValueChange(text, "password")}
+                                    value={fields.password.value}
                                 />
-                                <BorderlessButton 
-                                    onPress={() => setShowPassword(!showPassword)} 
+                                <BorderlessButton
+                                    onPress={() => setShowPassword(!showPassword)}
                                     style={styles.inputIcon}
                                 >
                                     <Image
@@ -78,14 +174,14 @@ function Login() {
 
                         <View style={styles.loginActions}>
                             <View style={styles.rememberUser}>
-                                <Checkbox 
-                                    active={rememberMe} 
-                                    onChange={() => setRememberMe(!rememberMe)} 
+                                <Checkbox
+                                    active={rememberUser}
+                                    onChange={() => setRememberUser(!rememberUser)}
                                 />
                                 <Text style={styles.rememberUserText}>Lembrar-me</Text>
                             </View>
 
-                            <BorderlessButton>
+                            <BorderlessButton onPress={() => navigation.navigate("forgot-password")}>
                                 <Text style={styles.forgotPasswordBtnText}>
                                     Esqueci minha senha
                                 </Text>
@@ -93,14 +189,29 @@ function Login() {
                         </View>
 
                         <RectButton
-                            style={[styles.submitLoginBtn, styles.submitLoginBtnActive]}
+                            style={[
+                                styles.submitLoginBtn,
+                                formValid && !authContext.loading
+                                    ? styles.submitLoginBtnActive
+                                    : styles.submitLoginBtnUnactive
+                            ]}
+                            enabled={formValid}
+                            onPress={login}
                         >
-                            <Text
-                                style={[
-                                    styles.submitLoginBtnText,
-                                    styles.submitLoginBtnTextActive
-                                ]}
-                            >Entrar</Text>
+                            {
+                                !authContext.loading
+                                    ? (
+                                        <Text
+                                            style={[
+                                                styles.submitLoginBtnText,
+                                                formValid
+                                                    ? styles.submitLoginBtnTextActive
+                                                    : styles.submitLoginBtnTextUnactive
+                                            ]}
+                                        >Entrar</Text>
+                                    )
+                                    : <ActivityIndicator />
+                            }
                         </RectButton>
                     </View>
                 </View>
