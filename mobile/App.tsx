a30a7fe525b67fdc92d7e54a1f30fb8b27dc3930
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-09-11 18:05:38 -0400

    Refactored to use authContext

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -8,9 +8,13 @@ import { StatusBar } from 'expo-status-bar'
 
 // Pages
 import Onboarding from 'pages/Onboarding'
+import Register from 'pages/Register'
 
 // Routes
 import AppStack from 'routes/AppStack'
+
+// Contexts
+import { useAuth, AuthProvider } from 'contexts/auth'
 
 // Fonts
 import {
@@ -20,9 +24,11 @@ import {
 } from '@expo-google-fonts/archivo'
 import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
 
-export default function App() {
-  const [component, setComponent] = useState<JSX.Element>(<AppStack />)
+function App() {
+  const [component, setComponent] = useState<JSX.Element>(<Register />)
   const [recheck, setRecheck] = useState(false)
+
+  const authContext = useAuth()
 
   const [fontsLoaded] = useFonts({
     Archivo_400Regular,
@@ -40,22 +46,31 @@ export default function App() {
     (async function checkFirstAppOpen() {
       if (fontsLoaded || recheck) {
         const check = await AsyncStorage.getItem('proffy:app:alreadyopened')
-        if (!check) setComponent(<Onboarding onDismiss={onOnboardingDismiss}/>)
-        else setComponent(<AppStack />)
+        if (!check) setComponent(<Onboarding onDismiss={onOnboardingDismiss} />)
+        else {
+          if (!authContext.signedIn) setComponent(<Register />)
+          else setComponent(<AppStack />)
+        }
       }
     })()
-  }, [fontsLoaded, recheck])
+  }, [fontsLoaded, recheck, authContext.signedIn])
 
   if (!fontsLoaded) return <AppLoading />
 
   return (
     <>
-      <StatusBar style="light" />
-      {component}
+      <StatusBar style="light" hidden />
+      { component }
     </>
   )
 }
 
-const styles = StyleSheet.create({
+function app() {
+  return (
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  )
+}
 
-})
+export default app
