e88a5ee178e5ba29dad0cf472c9f7edc4ea194ce
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-09-18 16:57:35 -0400

    Added route to class register feedback

--- mobile/src/routes/AppStack.tsx
+++ mobile/src/routes/AppStack.tsx
@@ -8,19 +8,34 @@ import { createStackNavigator } from '@react-navigation/stack'
 import Landing from 'pages/Landing'
 import TeacherForm from 'pages/TeacherForm'
 import AppTab from './AppTab'
+import ProcessFinished from 'components/ProcessFinished'
 
 const Stack = createStackNavigator()
 
 function AppStack() {
-    return (
-        <NavigationContainer>
-          <Stack.Navigator headerMode="none" initialRouteName="landing">
-            <Stack.Screen name="landing" component={Landing} />
-            <Stack.Screen name="main" component={AppTab} />
-            <Stack.Screen name="teacher-form" component={TeacherForm} />
-          </Stack.Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Stack.Navigator headerMode="none" initialRouteName="landing">
+        <Stack.Screen name="landing" component={Landing} />
+        <Stack.Screen name="main" component={AppTab} />
+        <Stack.Screen name="teacher-form" component={TeacherForm} />
+        <Stack.Screen name="class-registered">
+          {
+            () => (
+              <ProcessFinished
+                title="Cadastro salvo!"
+                description="Tudo certo, seu cadastro está
+                na nossa lista de professores. Agora é
+                só ficar de olho no seu WhatsApp."
+                redirectTo="landing"
+                actionLabel="Entendi"
+              />
+            )
+          }
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 export default AppStack
