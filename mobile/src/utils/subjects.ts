b8d6a56e57bfd1daa5cd6202421d9487b245e73c
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-09-18 16:58:14 -0400

    Created subject utility file

---
+++ mobile/src/utils/subjects.ts
@@ -0,0 +1,14 @@
+export const subjects = [
+    { value: "Artes", label: "Artes" },
+    { value: "Biologia", label: "Biologia" },
+    { value: "Educação Física", label: "Educação Física" },
+    { value: "Espanhol", label: "Espanhol" },
+    { value: "Física", label: "Física" },
+    { value: "Geografia", label: "Geografia" },
+    { value: "História", label: "História" },
+    { value: "Inglês", label: "Inglês" },
+    { value: "Literatura", label: "Literatura" },
+    { value: "Matemática", label: "Matemática" },
+    { value: "Português", label: "Português" },
+    { value: "Química", label: "Química" }
+]
