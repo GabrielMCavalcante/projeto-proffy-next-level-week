5f5c7a499a26305b12d90d2d514336cc1c9ab17d
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-09-22 17:01:56 -0400

    Created TeacherScheduleContainer styles

---
+++ mobile/src/components/TeacherItem/TeacherScheduleContainer/styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    scheduleTable: {
+        width: '100%',
+        paddingHorizontal: 12
+    },
+
+    tableHeader: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 30,
+        paddingVertical: 10
+    },
+
+    tableHeaderText: {
+        fontFamily: 'Poppins_400Regular',
+        fontSize: 12,
+        color: '#9C98A6'
+    },
+})
